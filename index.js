ff108dedb33c80db069a46e96f9cedbed6cb3227
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-05-23 14:32:38 +0600

    parts api

--- index.js
+++ index.js
@@ -19,42 +19,49 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
-    try {
-      await client.connect();
-      
-      const userCollection = client.db('facturer').collection('users');
+  try {
+    await client.connect();
+
+    const userCollection = client.db('facturer').collection('users');
+    const partsCollection = client.db('facturer').collection('parts');
 
 
 
-      app.put("/user/:email", async (req, res) => {
-        const email = req.params.email
-        const user = req.body
-        const filter = { email };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: user,
-        };
-        const result = await userCollection.updateOne(filter, updateDoc, options);
-        const token = jwt.sign({ email }, process.env.SECRET_ACCESS_TOKEN);
-  
-        res.send({ result, token })
-      })
-     
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const filter = { email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email }, process.env.SECRET_ACCESS_TOKEN);
 
-      
+      res.send({ result, token })
+    })
 
-    }finally {
+    app.get('/parts', async (req, res) => {
+      const parts = await partsCollection.find().toArray()
+      console.log(parts)
+      res.send(parts)
+    })
 
-    }
+
+
+
+  } finally {
+
   }
-  
-  run().catch(console.dir);
-  
+}
+
+run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send("Facturer Server is Running")
+
+app.get('/', (req, res) => {
+  res.send("Facturer Server is Running")
 })
 
-app.listen(port,()=>console.log("Listening port ", port))
+app.listen(port, () => console.log("Listening port ", port))
